0567d9f30c07096b67d7dc0a30048d3517b0bd9d
msuryaaaa <suryadika007@gmail.com>
2017-03-12 16:25:58 +0700

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // library standar yang digunakan untuk input dan otput
+using namespace std; // memberi tahu compiler untuk menggunakan std namespace
+
+int main () {
+	cout << "Hello World"; // menampilkan teks "Hello World"
+	cout << "I Love living in this world"; // menampilkan teks "I Love living in this world"
+	return 0; // mengakhiri program
+	
+}
